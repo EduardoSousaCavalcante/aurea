e9466b0a3809104d7d436a88b757afcdb23f6f2d
EduardoSousaCavalcante <eduardoscavalcante22@gmail.com>
2026-03-05 19:15:35 -0300

    Alteração no pdf do pedido

--- resources/views/pdf/pedido.blade.php
+++ resources/views/pdf/pedido.blade.php
@@ -21,31 +21,6 @@
         margin: 2px 0;
         text-align: right;
     }
-</style>
-<table class="cabecalho">
-    <tr>
-        <!-- LADO ESQUERDO (LOGO) -->
-        <td width="50%">
-            <img src="{{ public_path('images/logo.jpg') }}" width="180">
-        </td>
-
-        <!-- LADO DIREITO (6 LINHAS) -->
-        <td width="50%" class="dados-empresa">
-            <p><strong>DOCES AGUIA COMERCIO VAREJISTA<br/> E SOCIEDADE UNIPESSOAL LTDA</strong></p>
-            <p>DOCES AGUIA - AUREA LUCIA</p>
-            <p>CNPJ: 46.689.087/0001-57</p>
-            <p>IE: 136229694114</p>
-            <p>RUA FLOR DE CABOLO, N° 4, CASA 2, Bairro: AE. Caralho</p>
-            <p>São Paulo, SP, 08235360</p>
-        </td>
-
-    </tr>
-</table>
-<style>
-    .secao-dupla {
-        width: 100%;
-        margin-top: 20px;
-    }
 
     .secao-dupla p {
         font-weight: bold;
@@ -54,32 +29,83 @@
 
     .secao-dupla fieldset {
         border: 1px solid #000;
-        padding: 10px;
-        height: 40px;
+        padding: 5px;
+        height: 15px;
     }
 
     .secao-dupla td {
-        width: 50%;
-        padding-right: 10px;
+        padding-right: 5px;
         vertical-align: top;
     }
 </style>
 
-<table class="secao-dupla">
+<table class="cabecalho">
     <tr>
-        <td>
-            <p>Vendedor</p>
-            <fieldset>
-                Aurea
-            </fieldset>
+        <!-- LADO ESQUERDO (LOGO) -->
+        <td width="50%">
+            <img src="{{ public_path('images/logo.jpg') }}" width="180">
         </td>
 
-        <td>
-            <p>Motorista</p>
-            <fieldset>
-                Raimundo
-            </fieldset>
+        <!-- LADO DIREITO (DADOS EMPRESA) -->
+        <td width="50%" class="dados-empresa">
+            <p><strong>DOCES AGUIA COMERCIO VAREJISTA<br/>E SOCIEDADE UNIPESSOAL LTDA</strong></p>
+            <p>DOCES AGUIA - AUREA LUCIA</p>
+            <p>CNPJ: 46.689.087/0001-57</p>
+            <p>IE: 136229694114</p>
+            <p>RUA FLOR DE CABOLO, N° 4, CASA 2, Bairro: AE. Caralho</p>
+            <p>São Paulo, SP, 08235360</p>
         </td>
+    </tr>
+</table>
+
+<table width="100%" style="margin-bottom:20px;">
+    <tr>
+
+        <!-- SEÇÃO ESQUERDA -->
+        <td width="40%" style="vertical-align: top;">
+            <table class="secao-dupla" width="100%">
+                <tr>
+                    <td>
+                        <p>Data do Pedido</p>
+                        <fieldset>
+                            {{ date('d/m/Y', strtotime($pedido->created_at)) }}
+                        </fieldset>
+                    </td>
+
+                    <td>
+                        <p>Data da Entrega</p>
+                        <fieldset>
+                            {{ date('d/m/Y') }}
+                        </fieldset>
+                    </td>
+                </tr>
+            </table>
+        </td>
+
+        <!-- ESPAÇO -->
+        <td width="20%"></td>
+
+        <!-- SEÇÃO DIREITA -->
+        <td width="40%" style="vertical-align: top;">
+            <table class="secao-dupla" width="100%">
+                <tr>
+                    <td>
+                        <p>Vendedor</p>
+                        <fieldset>
+                            Aurea
+                        </fieldset>
+                    </td>
+
+                    <td>
+                        <p>Motorista/Entregador</p>
+                        <fieldset>
+                            Raimundo
+                        </fieldset>
+                    </td>
+                </tr>
+            </table>
+        </td>
+
     </tr>
 </table>
 
@@ -92,10 +118,13 @@
             <th>Total</th>
         </tr>
     </thead>
+
     <tbody>
+
         @php $totalGeral = 0; @endphp
 
         @foreach($pedido->produtos as $produto)
+
             @php
                 $totalItem = $produto->pivot->quantidade * $produto->pivot->preco_unitario;
                 $totalGeral += $totalItem;
@@ -107,9 +136,12 @@
                 <td>R$ {{ number_format($produto->pivot->preco_unitario, 2, ',', '.') }}</td>
                 <td>R$ {{ number_format($totalItem, 2, ',', '.') }}</td>
             </tr>
+
         @endforeach
+
     </tbody>
 </table>
+
 
 <h3 style="text-align: right;">
     Total Geral: R$ {{ number_format($totalGeral, 2, ',', '.') }}
